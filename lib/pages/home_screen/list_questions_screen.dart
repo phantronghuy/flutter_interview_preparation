5be97fe96de1e9e77a6c19e55269efa9603f4d92
phantronghuy <82666290+phantronghuy@users.noreply.github.com>
2022-09-21 16:08:33 +0700

    first commit

---
+++ lib/pages/home_screen/list_questions_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class ListQuetionsScreen extends StatefulWidget {
+  const ListQuetionsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ListQuetionsScreen> createState() => _ListQuetionsScreenState();
+}
+
+class _ListQuetionsScreenState extends State<ListQuetionsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size sizeOfScreen=MediaQuery.of(context).size;
+    return MaterialApp(
+      
+    );
+  }
+}
